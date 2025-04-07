e8b15f699afc9216fd26fc6f07bbc6b6389167af
Kayla Henderson <kaylahenderson0903@gmail.com>
2025-04-07 19:59:20 -0400

    File Commit

---
+++ my_module.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import random
+
+# Function that generates a plot
+def plot(func, x_values):
+   y_values = [func(x) for x in x_values]
+   plt.plot(x_values, y_values)
+   plt.xlabel('X-Values')
+   plt.ylabel('Y-Values')
+   plt.title('Function of X')
+   plt.show()
+   return True
+
+# Chooser Class
+class Chooser:
+    def __init__(self, list = [1, 2, 3, 4, 5, 6]):
+        self.list = list
+    def get_value(self):
+        print(self.list)
+    def select(self):
+        self.value1 = random.choice(self.list)
+        self.value2 = self.value1
+        print(self.value2)
+        return True
+
+# Solution to a Leetcode Problem (Palindrome Number)
+def palindrome_num(num):
+    if str(num) == str(num)[::-1]:
+        print(True)
+    else:
+        print(False)
+    return True
---
+++ test_my_module.py
@@ -0,0 +1,14 @@
+import my_module as mm # type: ignore
+import numpy as np
+
+# Tests Plot Function
+def test_plot():
+    assert mm.plot(lambda x: x**2, np.linspace(0, 5, 100))
+
+# Tests Chooser Class
+def test_chooser():
+    assert mm.Chooser([1, 2, 3, 4, 5, 6])
+
+# Tests Palindrome Number Function
+def test_palindrome():
+    assert mm.palindrome_num(121)
